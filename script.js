194e50372dbf2d9b555d4a47d1141690568a5628
Harpaavan Sahota <harpaavan93@gmail.com>
2026-08-12 23:08:38 -0600

    Started to add cursor effects using JS

---
+++ script.js
@@ -0,0 +1,30 @@
+const dot = document.querySelector('.cursor-dot');
+
+let mouseX = 0;
+let mouseY = 0;
+
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+function updateCursor() {
+    dot.style.transform = `translate(${mouseX}px, ${mouseY}px) translate(-50%, -50%)`;
+    requestAnimationFrame(updateCursor);
+}
+
+updateCursor();
+
+const link = document.querySelectorAll('a');
+
+link.forEach((link) => {
+    link.addEventListener('mouseenter', () => {
+        dot.classList.add('hover-active');
+    });
+});
+
+link.forEach((link) => {
+    link.addEventListener('mouseleave', () => {
+        dot.classList.remove('hover-active');
+    });
+});
