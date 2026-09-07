612491b52a5df7e6367e331b712e58bc9c4e338b
Harpaavan Sahota <harpaavan93@gmail.com>
2026-09-07 01:32:54 -0600

    Added console log to help with debugging

--- script.js
+++ script.js
@@ -62,6 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
             const imageURL = import.meta.env.BASE_URL + link.getAttribute('href').replace(/^\//, '');
+            console.log("Trying to load:", imageURL);
             modalImg.setAttribute("src", imageURL);
             modal.classList.add("open");
         });
